f3de6cff019d347d97de45442b35d641c46ff996
phong <phongprovn.wap.sh@gmail.com>
2020-05-17 01:29:42 +0700

    Add code

---
+++ app/src/test/java/ntp/internet/speed/meter/MathTest.kt
@@ -0,0 +1,76 @@
+package ntp.internet.speed.meter
+
+import org.junit.Before
+import org.junit.Test
+
+internal class MathTest {
+    var mMath = Math()
+    @Before
+    fun setUp() {
+
+    }
+
+    @Test
+    fun getMCalendar() {
+    }
+
+    @Test
+    fun getDate() {
+    }
+
+    @Test
+    fun getMonth() {
+    }
+
+    @Test
+    fun getYear() {
+        println(mMath.getYear())
+    }
+
+    @Test
+    fun getHour() {
+    }
+
+    @Test
+    fun getMinute() {
+    }
+
+    @Test
+    fun getSecond() {
+        println(mMath.getSecond())
+    }
+
+    @Test
+    fun getToDay() {
+        println(mMath.getToDay())
+    }
+
+    @Test
+    fun getTimeToDay() {
+        println(mMath.getTimeToDay())
+    }
+
+    @Test
+    fun getTotalRxBytes() {
+    }
+
+    @Test
+    fun getTotalTxBytes() {
+    }
+
+    @Test
+    fun getSpeedDownload() {
+    }
+
+    @Test
+    fun getSpeedUpload() {
+    }
+
+    @Test
+    fun kbToString() {
+    }
+
+    @Test
+    fun icon() {
+    }
+}
